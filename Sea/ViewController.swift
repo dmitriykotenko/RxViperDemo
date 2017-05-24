fdba041d5042864234996918059743130129805f
Dmitry Kotenko <d.kotenko@yandex.ru>
2017-05-24 14:53:39 +0300

    Initial Commit

---
+++ Sea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sea
+//
+//  Created by Dmitry Kotenko on 24/05/2017.
+//  Copyright © 2017 Tutu.ru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
